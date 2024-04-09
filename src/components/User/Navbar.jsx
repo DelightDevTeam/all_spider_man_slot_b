9bf2e55aad148bbc491227f3f41c3fcc8ddd6c61
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-04-09 20:55:41 +0630

    spiderman integrate done

--- src/components/User/Navbar.jsx
+++ src/components/User/Navbar.jsx
@@ -81,11 +81,37 @@ const Navbar = () => {
       <>
         <div className="navbar d-flex justify-content-between">
           <div>
-            <Link to={"/"}>
+            <Link to={"/"} className="text-decoration-none">
               <img className="logo" src={logo} />
+              <span className="text-white ms-3">Spider-Man</span>
             </Link>
           </div>
-          <div className="d-flex justify-content-center align-items-center">
+          {/* mobile */}
+          <div className="d-md-none d-sm-block">
+            <div className="d-flex justify-content-end align-items-center">
+              <div className="me-2">
+                <div className="d-flex">
+                  <span className="d-block">MM</span>
+                  <i
+                    className={`fas fa-toggle-${lan === "mm" ? "off" : "on"} d-block mx-2`}
+                    onClick={language}
+                    style={{ cursor: "pointer", fontSize: "20px" }}
+                  ></i>
+                  <span className="d-block">EN</span>
+                </div>
+              </div>
+              <div>
+                <a onClick={logOut} style={{ cursor: "pointer" }} className="p-2 text-white">
+                  {smallLoad && <BtnSpinner />}
+                  <i className="fas fa-right-from-bracket"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* mobile */}
+          {/* desktop */}
+          <div className="d-none d-md-flex justify-content-center align-items-center">
             <div className="me-3 d-flex">
               <span className="d-block">MM</span>
               <i
@@ -95,8 +121,7 @@ const Navbar = () => {
               ></i>
               <span className="d-block">EN</span>
             </div>
-            
-            <div className="border border-success rounded-3 px-2 py-1 me-2 m-auto">
+            <div className="px-2 py-1 me-2 m-auto">
               <Link
                 to="/profile"
                 className="text-decoration-none text-white me-3 text-center"
@@ -107,19 +132,39 @@ const Navbar = () => {
                 ></i>
                 <span>{user?.user_name}</span>
               </Link>
-              <span className="text-white d-block">
+              <span className="text-white">
                 <i className="fas fa-wallet me-2"></i>
                   K{parseFloat(user?.balance).toLocaleString()}
               </span>
               
             </div>
-            
-            <a onClick={logOut} style={{ cursor: "pointer" }} className="p-2 text-white">
+            <a onClick={logOut} style={{ cursor: "pointer" }} className="p-2 text-white btn btn-sm btn-outline-light">
               {smallLoad && <BtnSpinner />}
-              <i className="fas fa-right-from-bracket"></i>
+              <i className="fas fa-right-from-bracket me-2"></i>
+              Logout
             </a>
           </div>
+          {/* desktop */}
         </div>
+        
+        <div className="d-md-none d-sm-block">
+        <hr className="pb-0 mb-0" />
+          <div className="d-flex justify-content-between align-items-center pt-3 px-3">
+              <div>
+                <Link to="/profile" className="text-decoration-none text-white me-3 text-center">
+                  <i className="fa-regular fa-user-circle me-2" style={{ fontSize: "20px" }}></i>
+                  <span>{user?.user_name}</span>
+                </Link>
+              </div>
+              <div>
+                  <span className="text-white">
+                    <i className="fas fa-wallet me-2"></i>
+                      K{parseFloat(user?.balance).toLocaleString()}
+                  </span>
+              </div>
+          </div>
+        </div>
+
       </>
     )}
 
